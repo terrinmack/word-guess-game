a081bab9aac056aa8747612e9a60a5f74a2b3737
Terri Mack <terrimack15@gmail.com>
2023-01-19 21:11:32 -0800

    added word list in java

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+var wordList = [
+    {
+        word: "potato",
+        hint: "french fry"
+    },
+    {
+        word: "javascript",
+        hint: "programming language"
+    },
+    {
+        word: "shortcake",
+        hint: "a type of dessert containing strawberries"
+    },
+    {
+        word: "moogle",
+        hint: "a fluffy, white, floating creature from the game series Final Fantasy"
+    },
+    {
+        word: "Hydaelyn",
+        hint: "an old woman embedded in a crystal in space in the game Final Fantasy XIV"
+    },
+    {
+        word: "Emet",
+        hint: "the first name of the salt and pepper haired male in the game Final Fantasy XIV"
+    },
+    {
+        word: "dog",
+        hint: "it's not god"
+    },
+]
+
+var inputs = document.querySelector(".inputs")
